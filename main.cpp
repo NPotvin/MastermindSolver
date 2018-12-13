545872e94cbe2d6179a20e35ec0dc3aeb7130616
BeanOfChaos <benjarte@ulb.ac.be>
2018-12-13 18:01:25 +0100

    fix size of buffers (hopefully)

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &tot_proc);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
     if (tot_proc <= 1) {
       std::cout << "Master can't play alone!" << std::endl;
       MPI_Abort(MPI_COMM_WORLD, 1);
@@ -28,7 +27,7 @@ int main(int argc, char** argv) {
 
     if (!rank) {
       master = new GameMaster(rand);
-      playersGuesses = new Guess[tot_proc - 1];
+      playersGuesses = new Guess[tot_proc];
     }
     else{
       challenger = new Challenger;
@@ -41,17 +40,17 @@ int main(int argc, char** argv) {
       }
       if (!rank) std::cout<< "Collecting guesses!" << std::endl;
       MPI_Gather((void*) playersGuesses, sizeof(Guess), MPI_BYTE,
-                 (void*) playersGuesses, sizeof(Guess)*(tot_proc -1) , MPI_BYTE,
+                 (void*) playersGuesses, sizeof(Guess)*(tot_proc) , MPI_BYTE,
                  0, MPI_COMM_WORLD);
       ++guess_count;
       if(!rank) {
         std::cout << "Evaluating random guess: " << std::endl ;
-        evaluated_guess = master->manageGuesses(playersGuesses, static_cast<const size_t>(tot_proc - 1));
+        evaluated_guess = master->manageGuesses(&(playersGuesses[1]), static_cast<const size_t>(tot_proc - 1));
         done = master->isGameFinished();
       }
       MPI_Bcast((void*) &evaluated_guess, sizeof(Result),
                 MPI_BYTE, 0, MPI_COMM_WORLD);
+      challenger->updatePlausibleGuesses(evaluated_guess);
+      done = evaluated_guess.getPerfect() == GUESS_SIZE() ;
      }
-     challenger->updatePlausibleGuesses(evaluated_guess);
-     done = evaluated_guess.getPerfect() == GUESS_SIZE() ;
 }
